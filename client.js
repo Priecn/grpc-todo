9361860ffd71a983ea822ef4d1dca9945591e0bd
Priecn <prince.silverarrow@gmail.com>
2022-02-03 22:38:27 +0530

    unary mode example

---
+++ client.js
@@ -0,0 +1,22 @@
+const grpc = require("grpc");
+const protoLoader = require("@grpc/proto-loader");
+
+const packageDef = protoLoader.loadSync("todo.proto", {});
+const grpcObj = grpc.loadPackageDefinition(packageDef);
+
+const todoPackage = grpcObj.todoPackage;
+
+const client = new todoPackage.Todo("localhost:40000", grpc.credentials.createInsecure());
+
+const todoText = process.argv[2];
+
+client.createTodo({
+    "id": -1,
+    "text": todoText
+}, (err, response) => {
+    console.log("Response: " + JSON.stringify(response));
+})
+
+client.readTodos({}, (err, response) => {
+    console.log("Todos: " + JSON.stringify(response));
+});
---
+++ server.js
@@ -0,0 +1,32 @@
+const grpc = require("grpc");
+const protoLoader = require("@grpc/proto-loader");
+
+const packageDef = protoLoader.loadSync("todo.proto", {});
+const grpcObj = grpc.loadPackageDefinition(packageDef);
+
+const todoPackage = grpcObj.todoPackage;
+
+const server = new grpc.Server();
+server.bind("0.0.0.0:40000", grpc.ServerCredentials.createInsecure());
+
+server.addService(todoPackage.Todo.service, {
+    "createTodo": createNewTodo,
+    "readTodos": readAllTodo
+});
+
+server.start();
+
+const todos = [];
+
+function createNewTodo(call, callback) {
+    const todoItem = {
+        "id" : todos.length + 1,
+        "text" : call.request.text
+    };
+    todos.push(todoItem);
+    callback(null, todoItem);
+}
+
+function readAllTodo(call, callback) {
+    callback(null, {"items": todos});
+}
